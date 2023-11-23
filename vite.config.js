e021fc188364c5c62d8ff10514a40e5f42acdbd2
HerwayEdwins <127233583+HerwayEdwins@users.noreply.github.com>
2023-11-23 15:37:02 +0300

    Updated the resources for JS & CSS for scoreboard page
    The JS ,CSS link to the Scoreboard and report page

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/css/stylee.css','resources/js/script-easy.js','resources/js/script-medium.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/css/stylee.css','resources/js/script-easy.js','resources/js/script-medium.js','resources/js/script-hard.js','resources/js/scoreboard.js'],
             refresh: true,
         }),
     ],
 });
+
